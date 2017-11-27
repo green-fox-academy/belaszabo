7feea6d5d7912c106c52d01e02a33099853708f2
Bela Szabo <belapontszabo@gmail.com>
2017-11-27 12:47:19 +0100

    add 09

--- week-02/day-01/define_basic_info.py
+++ week-02/day-01/define_basic_info.py
@@ -3,3 +3,11 @@
 # Your age as an integer
 # Your height in meters as a double
 # Whether you are married or not as a boolean
+name = "Bela"
+age = 26
+height = 1.85
+marriage = False
+print(name)
+print(age)
+print(height)
+print(marriage)
