f39c6384abe5838589e2917ec628fb2473407b98
Bela Szabo <belapontszabo@gmail.com>
2017-11-27 16:42:15 +0100

    add 20

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,1 +1,7 @@
 # Write a program that asks for two numbers and prints the bigger one
+a = int(input('Write a number: '))
+b = int(input('Write another number: '))
+if a > b:
+    print(a)
+else:
+    print(b)
