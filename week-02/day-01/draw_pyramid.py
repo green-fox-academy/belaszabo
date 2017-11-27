473c4df958a7e64311c40f12814d4bb8871f8100
Bela Szabo <belapontszabo@gmail.com>
2017-11-27 17:30:28 +0100

    change 29

--- week-02/day-01/draw_pyramid.py
+++ week-02/day-01/draw_pyramid.py
@@ -1,13 +1,5 @@
-# Write a program that reads a number from the standard input, then draws a
-# pyramid like this:
-#
-#
-#    *
-#   ***
-#  *****
-# *******
-#
-# The pyramid should have as many lines as the number was
 a = int(input())
-for i in range(1,a,2):
-    print(' ' * (i-1) + '*' *i)
+b = a - 1
+for i in range(1, a + a * 1, 2):
+    print(' ' * b + '*' * i )
+    b -= 1
