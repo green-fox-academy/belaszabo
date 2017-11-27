f54c9590bd0805fbb9b15fc56563e867028ba9ba
Bela Szabo <belapontszabo@gmail.com>
2017-11-27 16:36:59 +0100

    add 17

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,3 +2,11 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+d = float(input('d = '))
+e = float(input('e = '))
+sum = a + b + c + d + e
+z = sum / 5
+print('Sum:',sum,', Average:',z)
