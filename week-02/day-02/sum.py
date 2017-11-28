12faaf140a33cf2e71bd537de6b9e78d5347b103
Bela Szabo <belapontszabo@gmail.com>
2017-11-28 11:35:18 +0100

    add sum.py

--- week-02/day-02/sum.py
+++ week-02/day-02/sum.py
@@ -1,2 +1,8 @@
 # - Write a function called `sum` that sum all the numbers
 #   until the given parameter
+def sum(number):
+    total = 0
+    for i in range(0,number+1):
+        total += i
+    print(total)
+sum(4)
