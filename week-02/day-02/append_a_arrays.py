f8c98df73b4061f798b0a91d6791b4daa405ac90
Bela Szabo <belapontszabo@gmail.com>
2017-11-28 16:25:11 +0100

    modified append_a_arrays.py.py

--- week-02/day-02/append_a_arrays.py
+++ week-02/day-02/append_a_arrays.py
@@ -2,6 +2,6 @@
 #   with the following content: `["kuty", "macsk", "cic"]`
 # - Add all elements an `"a"` at the end
 nimals = ["kuty", "macsk", "cic"]
-for i in range(0,len(nimals)):
-    nimals[i] =  nimals[i] + "a"
+for i in range(len(nimals)):
+    nimals[i] += "a"
 print(nimals)
--- week-02/day-02/diagonal_matrix.py
+++ week-02/day-02/diagonal_matrix.py
@@ -7,3 +7,7 @@
 #   0 0 0 1
 #
 # - Print this two dimensional list to the output
+for i in range(4):
+    a = [0]
+    a[i] = 1
+    print(a)
