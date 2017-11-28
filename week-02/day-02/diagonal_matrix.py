45c68954b0653f2a3a52fa7b35d292b0e9f6d213
Bela Szabo <belapontszabo@gmail.com>
2017-11-28 16:53:28 +0100

    add diagonal_matrix.py

--- week-02/day-02/diagonal_matrix.py
+++ week-02/day-02/diagonal_matrix.py
@@ -7,7 +7,24 @@
 #   0 0 0 1
 #
 # - Print this two dimensional list to the output
-for i in range(4):
-    a = [0]
-    a[i] = 1
-    print(a)
+
+#for i in range(4):
+#    a = [0]
+#    a[i] = 1
+#    print(a)
+
+a = []
+b = []
+size = 4
+ 
+for i in range(size):
+    b = []
+    for j in range(size):
+        if i == j: 
+            b.append('1')
+        else:
+            b.append('0')
+    a.append(b)
+
+for i in range(size):
+     print(a[i])
