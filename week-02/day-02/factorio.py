53b28897e87fb5b7dec573440c0e0bb4c9445e62
Bela Szabo <belapontszabo@gmail.com>
2017-11-28 11:49:40 +0100

    add factorio.py

--- week-02/day-02/factorio.py
+++ week-02/day-02/factorio.py
@@ -1,2 +1,9 @@
 # - Create a function called `factorio`
 #   that returns it's input's factorial
+def factorio(number):
+    total = 1
+    for i in range(1,number+1):
+        total *= i
+    return total
+
+print(factorio(7))
