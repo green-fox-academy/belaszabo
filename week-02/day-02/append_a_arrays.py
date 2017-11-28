d0d8177af7196f38d8df132befe0780b671ba1c6
Bela Szabo <belapontszabo@gmail.com>
2017-11-28 12:39:46 +0100

    add append_a_arrays.py

---
+++ week-02/day-02/append_a_arrays.py
@@ -0,0 +1,7 @@
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macsk", "cic"]`
+# - Add all elements an `"a"` at the end
+nimals = ["kuty", "macsk", "cic"]
+for i in range(0,len(nimals)):
+    nimals[i] =  nimals[i] + "a"
+print(nimals)
