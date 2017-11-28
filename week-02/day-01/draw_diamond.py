ce7cf1ee42284b12f89ff1a9531c99498fb21122
Bela Szabo <belapontszabo@gmail.com>
2017-11-28 11:20:03 +0100

    add doubling.py

--- week-02/day-01/draw_diamond.py
+++ week-02/day-01/draw_diamond.py
@@ -11,3 +11,12 @@
 #    *
 #
 # The diamond should have as many lines as the number was
+a = int(input())
+b = a - 1
+c = a * 2 -1
+for i in range(1, a + a * 1, 2):
+    print(' ' * b + '*' * i )
+    b -= 1
+for o in range(1, a + a * 1, 2):
+    print(' ' * (o-2) + '*' * c )
+    c -= 2
