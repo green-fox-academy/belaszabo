b0e47ce76de4635c277ee3a1748446c7a59da36b
Bela Szabo <belapontszabo@gmail.com>
2017-11-28 12:40:56 +0100

    add double_items.py

--- week-02/day-02/double_items.py
+++ week-02/day-02/double_items.py
@@ -2,7 +2,7 @@
 #   with the following content: `[3, 4, 5, 6, 7]`
 # - Double all the values in the array
 ag = [3, 4, 5, 6, 7]
-for i in len(ag):
-    dag = ag[i] * 2
+for i in range(0,len(ag)):
+    ag[i] = ag[i] * 2
     
 print(ag)
