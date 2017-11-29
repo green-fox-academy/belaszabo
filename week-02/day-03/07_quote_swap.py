8cabff5c68c4838440e6e4b53c6ec5b767eaddd9
Bela Szabo <belapontszabo@gmail.com>
2017-11-29 12:30:33 +0100

    07_quote_swap.py

---
+++ week-02/day-03/07_quote_swap.py
@@ -0,0 +1,12 @@
+# Accidentally I messed up this quote from Richard Feynman.
+# Two words are out of place
+# Your task is to fix it by swapping the right words with code
+
+# Also, print the sentence to the output with spaces in between.
+
+words = ["What", "I", "do", "create,", "I", "cannot", "not", "understand."]
+q = ""
+words[2], words[5] = words[5], words[2]
+for i in range(0,len(words)):
+    q += words[i] + " "
+print(q)
