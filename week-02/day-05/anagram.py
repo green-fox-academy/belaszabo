00fde9e75667558f7aedcc4a49c81b1fcf7b2811
Bela Szabo <belapontszabo@gmail.com>
2017-12-01 11:25:52 +0100

    modified anagram.py

--- week-02/day-05/anagram.py
+++ week-02/day-05/anagram.py
@@ -5,14 +5,9 @@ def anagram(str1, str2):
         str1_list.append(str1[i])
     for i in range(0,len(str2)):
         str2_list.append(str2[i])
-    tmp1 = False
-    tmp2= False
-    if len(str1) == len(str2):
-        for i in str1_list:
-            if i in str2_list:
-                tmp1 = True
-        for i in str2_list:
-            if i in str1_list:
-                tmp2 = True
-        return True if tmp1 == True and tmp2 == True else False
-print(anagram("gyomorrontas", "toronymorgas"))
+    if str1_list.sort == str2_list.sort:
+        return True
+    else:
+        return False
+
+print(anagram("anna", "annn"))
