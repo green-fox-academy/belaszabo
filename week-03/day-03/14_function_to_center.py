06a25308d2803eac5aadd14ecd3b5d03b03e7328
Bela Szabo <belapontszabo@gmail.com>
2017-12-06 16:41:39 +0100

    modified exercise 14

--- week-03/day-03/14_function_to_center.py
+++ week-03/day-03/14_function_to_center.py
@@ -10,8 +10,7 @@ canvas.pack()
 # and draws a line from that point to the center of the canvas.
 # fill the canvas with lines from the edges, every 20 px, to the center.
 def drawer():
-    diagonal = canvas.create_line(300, 0, 0, 300)
-    for i in range(15):
+    for i in range(16):
         a = 20
         line1 = canvas.create_line(0+i*a, 0, 300-i*a, 300)
         line2 = canvas.create_line(0, 0+i*a, 300, 300-i*a)
