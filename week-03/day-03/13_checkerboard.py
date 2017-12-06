ea29f18b7d4091f7e323410ab274414b578f3af2
Bela Szabo <belapontszabo@gmail.com>
2017-12-06 16:15:47 +0100

    add 13_checkerboard.py

--- week-03/day-03/13_checkerboard.py
+++ week-03/day-03/13_checkerboard.py
@@ -7,13 +7,23 @@ canvas.pack()
 
 # fill the canvas with a checkerboard pattern.
 
-def draw_lines(x, y):
+def draw_board(a):
     for i in range(8):
-        line1 = canvas.create_line(x, 0, x, 300)
-        line2 = canvas.create_line(0, y, 300, y)
-        x += 40
-        y += 40
-
-draw_lines(40,40)
+        y = 0
+        y += i * a
+        if i % 2 == 0:
+            for i in range(4):
+                x = 0
+                x += i * a * 2
+                black = canvas.create_rectangle(x, y, x + a, y + a, fill = 'black')
+                white = canvas.create_rectangle(x + a, y, x + 2 * a, y + a, fill = 'white')
+        else:
+            for i in range(4):
+                x = 0
+                x += i * a * 2
+                black = canvas.create_rectangle(x, y, x + a, y + a, fill = 'white')
+                white = canvas.create_rectangle(x + a, y, x + 2 * a, y + a, fill = 'black')
+        
+draw_board(37)
 
 root.mainloop()
