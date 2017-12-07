30ee47d61d81c9d636577bb6e64bcf45ea36f466
Bela Szabo <belapontszabo@gmail.com>
2017-12-07 11:00:17 +0100

    add 04_power.py

---
+++ week-03/day-04/04_power.py
@@ -0,0 +1,10 @@
+# Given base and n that are both 1 or more, compute recursively (no loops)
+# the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+def power(base, n):
+    if n == 0:
+        return 1
+    else:
+        return base * power(base, n-1)
+
+print(power(2, 3))
+print(2**3)
