ca8ebcbef87bcbb6768295e3d53f930a575557c5
Bela Szabo <belapontszabo@gmail.com>
2017-12-13 12:40:03 +0100

    add 02

--- week-04/day-03/bela_test.py
+++ week-04/day-03/bela_test.py
@@ -1,5 +1,5 @@
 import unittest
-from bela_work import *
+from bela_work import Apple
 
 class TestBelaWork(unittest.TestCase):
 
@@ -7,6 +7,29 @@ class TestBelaWork(unittest.TestCase):
         apple = Apple()
         self.assertEqual(apple.get_apple(), "apple")
 
+
+    def test_summa_empty_list(self):
+        apple = Apple()
+        test_list = []
+        self.assertEqual(apple.summa(test_list), 0)
+
+
+    def test_summa_one_number(self):
+        apple = Apple()
+        test_list = [2]
+        self.assertEqual(apple.summa(test_list), 2)
+
+
+    def test_summa_multiple_numbers(self):
+        apple = Apple()
+        test_list = [2, 4, 5]
+        self.assertEqual(apple.summa(test_list), 11)
+
+
+    def test_summa_null(self):
+        apple = Apple()
+        test_list = ["null"]
+        self.assertEqual(apple.summa(test_list), "Enter a list of numbers")
 
 if __name__ == '__main__':
     unittest.main()
--- week-04/day-03/bela_work.py
+++ week-04/day-03/bela_work.py
@@ -2,3 +2,16 @@ class Apple(object):
 
     def get_apple(self):
         return "apple"
+
+
+    def summa(self, list_of_integers):
+        total = 0
+        if list_of_integers != []:
+            for number in list_of_integers:
+                if type(number) == int:
+                    total += number
+                else:
+                    return "Enter a list of numbers"
+        else:
+            total = 0
+        return total
