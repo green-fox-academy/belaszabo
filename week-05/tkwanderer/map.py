5bd9ec90d216b81a28af29599e3d7c5ea5c35a27
Bela Szabo <belapontszabo@gmail.com>
2017-12-18 12:42:29 +0100

    mod(view) modified tile drawing

--- week-05/tkwanderer/map.py
+++ week-05/tkwanderer/map.py
@@ -8,16 +8,19 @@ class Tile(object):
     def __init__(self):
         pass
 
-    def draw_floor_tile(self):
+    def draw_floor_tile(self, x, y):
         root = Tk()
         canvas = Canvas(root, width = 720, height = 720)
+        canvas.pack()
         
+        self.x = x
+        self.y = y
+
         tile = PhotoImage(file = "floor.png")
-        label = Label(root, image = tile)
-        label.pack()
+        canvas.create_image(self.x, self.y, anchor = NW, image = tile)
 
         root.mainloop()
 
 image = Tile()
-image.draw_floor_tile()
+image.draw_floor_tile(0, 0)
 
