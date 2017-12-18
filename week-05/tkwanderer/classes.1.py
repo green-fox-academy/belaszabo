01b044b6a37e537570d6272111ed0331c512a9dc
Bela Szabo <belapontszabo@gmail.com>
2017-12-18 17:11:01 +0100

    feat(hero) hero can not move on walls

--- week-05/tkwanderer/classes.1.py
+++ week-05/tkwanderer/classes.1.py
@@ -44,37 +44,37 @@ class Hero(Character):
 
     def move(self, e):
         if e.keysym == "Right":
-            if self.pos_x <= 576:
-                self.pos_x += 72
+            if self.pos_x <= 9 and screen.map_tiles[self.pos_y][self.pos_x + 1] == 0:
+                self.pos_x += 1
                 self.hero_image = PhotoImage(file = "hero-right.png") 
-                canvas.create_image(self.pos_x, self.pos_y, anchor = NW, image = self.hero_image)
+                canvas.create_image(self.pos_x * self.size, self.pos_y * self.size, anchor = NW, image = self.hero_image)
             else:
                 self.hero_image = PhotoImage(file = "hero-right.png") 
-                canvas.create_image(self.pos_x, self.pos_y, anchor = NW, image = self.hero_image)
+                canvas.create_image(self.pos_x * self.size, self.pos_y * self.size, anchor = NW, image = self.hero_image)
         elif e.keysym == "Left":
-            if self.pos_x >= 72:
+            if self.pos_x >= 1 and screen.map_tiles[self.pos_y][self.pos_x - 1] == 0:
                 self.hero_image = PhotoImage(file = "hero-left.png")
-                self.pos_x -= 72
-                canvas.create_image(self.pos_x, self.pos_y, anchor = NW, image = self.hero_image)
+                self.pos_x -= 1
+                canvas.create_image(self.pos_x * self.size, self.pos_y * self.size, anchor = NW, image = self.hero_image)
             else:
                 self.hero_image = PhotoImage(file = "hero-left.png")
-                canvas.create_image(self.pos_x, self.pos_y, anchor = NW, image = self.hero_image)
+                canvas.create_image(self.pos_x * self.size, self.pos_y * self.size, anchor = NW, image = self.hero_image)
         elif e.keysym == "Down":
-            if self.pos_y <= 576:
-                self.pos_y += 72
+            if self.pos_y <= 9 and screen.map_tiles[self.pos_y + 1][self.pos_x] == 0:
+                self.pos_y += 1
                 self.hero_image = PhotoImage(file = "hero-down.png") 
-                canvas.create_image(self.pos_x, self.pos_y, anchor = NW, image = self.hero_image)
+                canvas.create_image(self.pos_x * self.size, self.pos_y * self.size, anchor = NW, image = self.hero_image)
             else:
                 self.hero_image = PhotoImage(file = "hero-down.png") 
-                canvas.create_image(self.pos_x, self.pos_y, anchor = NW, image = self.hero_image)
+                canvas.create_image(self.pos_x * self.size, self.pos_y * self.size, anchor = NW, image = self.hero_image)
         elif e.keysym == "Up":
-            if self.pos_y >= 72:
-                self.pos_y -= 72
+            if self.pos_y >= 1 and screen.map_tiles[self.pos_y - 1][self.pos_x] == 0:
+                self.pos_y -= 1
                 self.hero_image = PhotoImage(file = "hero-up.png") 
-                canvas.create_image(self.pos_x, self.pos_y, anchor = NW, image = self.hero_image)
+                canvas.create_image(self.pos_x * self.size, self.pos_y * self.size, anchor = NW, image = self.hero_image)
             else:
                 self.hero_image = PhotoImage(file = "hero-up.png") 
-                canvas.create_image(self.pos_x, self.pos_y, anchor = NW, image = self.hero_image)
+                canvas.create_image(self.pos_x * self.size, self.pos_y * self.size, anchor = NW, image = self.hero_image)
 
 class Enemy(Character):
     pass
--- week-05/tkwanderer/main.py
+++ week-05/tkwanderer/main.py
@@ -1,9 +1,14 @@
 from tkinter import *
-from classes import MainScreen
+from classes import MainScreen, Character, Hero
 
 root = Tk()
 canvas = Canvas(root, width = 720, height = 720)
 canvas.pack()
+
+hero = Hero()
+canvas.bind("<KeyPress>", hero.move)
+
+canvas.focus_set()
 
 screen = MainScreen()
 screen.draw_table()
