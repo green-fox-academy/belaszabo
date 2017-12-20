e4338c3be6db1eca8103c86e8705ef9bebbd8889
Bela Szabo <belapontszabo@gmail.com>
2017-12-20 14:42:22 +0100

    add current and max hp

--- week-05/tkwanderer/classes.py
+++ week-05/tkwanderer/classes.py
@@ -2,7 +2,7 @@ from tkinter import *
 from random import randint
 
 root = Tk()
-canvas = Canvas(root, width = 800, height = 720)
+canvas = Canvas(root, width = 820, height = 720)
 canvas.pack()
 
 class MainScreen(object):
@@ -37,19 +37,18 @@ class Character(object):
         self.pos_y = 0
         self.size = 72
         self.level = 1
-        self.hp = 0
+        self.max_hp = 0
+        self.current_hp = 0
         self.dp = 0
         self.sp = 0
-
-    def strike(self, e):
-        pass
 
 class Hero(Character):
     def __init__(self):
         super().__init__()
         self.hero_image = PhotoImage(file = "hero-down.png") 
         canvas.create_image(self.pos_x, self.pos_y, anchor = NW, image = self.hero_image)
-        self.hp = 20 + 3 * randint(1, 6)
+        self.max_hp = 20 + 3 * randint(1, 6)
+        self.current_hp = self.max_hp
         self.dp = 2 * randint(1, 6)
         self.sp = 5 + randint(1, 6)
 
@@ -92,12 +91,12 @@ class Hero(Character):
         self.dp += randint(1, 6)
         self.sp += randint(1, 6)
 
-
 class Enemy(Character):
     def __init__(self):
         super().__init__()
         self.skeleton_image = PhotoImage(file = "skeleton.png")
-        self.hp = 2 * self.level * randint(1, 6)
+        self.max_hp = 2 * self.level * randint(1, 6)
+        self.current_hp = self.max_hp
         self.dp = self.level / 2 * randint(1, 6)
         self.sp = self.level * randint(1, 6)
 
@@ -115,7 +114,8 @@ class EnemyBoss(Enemy):
     def __init__(self):
         super().__init__()
         self.boss_image = PhotoImage(file = "boss.png")
-        self.hp = 2 * self.level * randint(1, 6) + randint(1, 6)
+        self.max_hp = 2 * self.level * randint(1, 6) + randint(1, 6)
+        self.current_hp = self.max_hp
         self.dp = self.level / 2 * randint(1, 6) + (randint(1, 6) / 2)
         self.sp = self.level * randint(1, 6) + self.level
     
@@ -134,11 +134,23 @@ class StatBox(object):
         canvas.create_text(750, 20, text = "Hero lvl:")
         canvas.create_text(790, 20, text = hero.level)
         canvas.create_text(750, 40, text = "Hero HP:")
-        canvas.create_text(790, 40, text = hero.hp)
+        canvas.create_text(790, 40, text = str(hero.current_hp) + " / " + str(hero.max_hp))
         canvas.create_text(750, 60, text = "Hero DP:")
         canvas.create_text(790, 60, text = hero.dp)
         canvas.create_text(750, 80, text = "Hero SP:")
         canvas.create_text(790, 80, text = hero.sp)
+
+    def fighting(self):
+        canvas.create_text(750, 120, text = "FIGHTING!!!")
+        canvas.create_text(750, 140, text = "Enemy's HP:")
+
+class Fight(object):
+    def __init__(self):
+        pass
+
+    def is_hero_on_enemy_tile(self):
+        if hero.pos_x == boss.pos_x and hero.pos_y == boss.pos_y:
+            stat_box.fighting()
 
 hero = Hero()
 canvas.bind("<KeyPress>", hero.move)
@@ -157,5 +169,8 @@ boss = EnemyBoss()
 boss.draw_boss()
 
 stat_box = StatBox()
+
+fight = Fight()
+fight.is_hero_on_enemy_tile()
 
 root.mainloop()
