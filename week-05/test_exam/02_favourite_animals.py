4c799295d59fda54888bd30d941be97799caaa62
Bela Szabo <belapontszabo@gmail.com>
2017-12-21 12:23:33 +0100

    add exercise 2

--- week-05/test_exam/02_favourite_animals.py
+++ week-05/test_exam/02_favourite_animals.py
@@ -7,3 +7,33 @@
 # One animal should be stored only at once
 # Each animal should be written in separate lines
 # The program should only save animals, no need to print them
+
+from sys import argv
+
+class FavouriteAnimal(object):
+
+    def __init__(self):
+        self.file_name = "favourites.txt"
+        if len(argv) == 1:
+            self.usage()
+        else:
+            self.is_in_file()
+
+    def usage(self):
+        print("02_favourite_animals.py [animal] [animal]...")
+        
+    def is_in_file(self):
+        # print("checking if in file")
+        f = open(self.file_name, "r")
+        lines = f.readlines()
+        f.close()
+        for i in range(1, len(argv)):
+            if argv[i] + "\n" not in lines:
+                self.add_fav(argv[i])
+
+    def add_fav(self, arg):
+        f = open(self.file_name, "a")
+        f.write(arg + "\n")
+        f.close()
+
+favs = FavouriteAnimal()
