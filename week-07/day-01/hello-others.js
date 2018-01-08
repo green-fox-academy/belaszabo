d938a094fb343953d94475d697207627ae9238ee
Bela Szabo <belapontszabo@gmail.com>
2018-01-08 11:48:41 +0100

    add hello-others.js

---
+++ week-07/day-01/hello-others.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+console.log('Hello, Hedi!')
+console.log('Hello, Patrik!')
+console.log('Hello, Fruzsi!')
