51d775b4a527a32b11456afd497dab42a6b94dee
Bela Szabo <belapontszabo@gmail.com>
2018-01-11 15:46:00 +0100

    feat(main) add arrow and keyboard navigation functions

--- week-07/day-04/gallery.js
+++ week-07/day-04/gallery.js
@@ -1,31 +1,63 @@
 'use strict';
 
-let leftArrow = document.querySelector('.cls-1');
-let rightArrow = document.querySelector('.cls-2');
+let leftArrow = document.querySelector('.left-arrow svg');
+let rightArrow = document.querySelector('.right-arrow svg');
 let pictures = document.querySelectorAll('img');
 let mainPicture = document.querySelector('.main-image');
+let source;
 
-//mukodo verzio, background beallitassal:
-// for (let i = 0; i < pictures.length; i++) {
-//   pictures[i].addEventListener('click', function(event) {
-//     let source = pictures[i].getAttribute('src');
-//     mainPicture.setAttribute('style', 'background-image: url(' + source +')');
-//   });
-// }
-
-let currentPicture = mainPicture.getElementsByTagName('img')[0];
+const currentPicture = mainPicture.getElementsByTagName('img')[0];
+let currentPictureIndex = 0;
 
 for (let i = 0; i < pictures.length; i++) {
   pictures[i].addEventListener('click', function(event) {
-    let source = pictures[i].getAttribute('src');
+    source = pictures[i].getAttribute('src');
     currentPicture.setAttribute('src', source);
+    currentPictureIndex = [i];
   });
 }
 
-// function nextPic(index) {
-//   mainPicture.removeChild();
-//   mainPicture.appendChild(pictures[index + 1 ]);
-//   currentPicture = pictures[index + 1];
-// }
+function nextPic() {
+  if (currentPictureIndex === 8) {
+    currentPictureIndex = 1;
+    let source = pictures[currentPictureIndex].getAttribute('src');
+    currentPicture.setAttribute('src', source);
+  } else if (currentPictureIndex === 0) {
+    currentPictureIndex = 2;
+    let source = pictures[currentPictureIndex].getAttribute('src');
+    currentPicture.setAttribute('src', source);
+  } else {
+    currentPictureIndex++;
+    let source = pictures[currentPictureIndex].getAttribute('src');
+    currentPicture.setAttribute('src', source);
+  }
+}
 
-// showFirstPic();
+function prevPic() {
+  if (currentPictureIndex === 1) {
+    currentPictureIndex = 8;
+    let source = pictures[currentPictureIndex].getAttribute('src');
+    currentPicture.setAttribute('src', source);
+  } else {
+    currentPictureIndex--;
+    let source = pictures[currentPictureIndex].getAttribute('src');
+    currentPicture.setAttribute('src', source);
+  }
+}
+
+rightArrow.addEventListener('click', nextPic);
+leftArrow.addEventListener('click', prevPic);
+
+
+function checkKey(e) {  
+  e = e || window.event;
+
+  if (e.keyCode == '37') {
+    prevPic();
+  }
+  else if (e.keyCode == '39') {
+    nextPic();
+  }  
+}
+
+document.onkeydown = checkKey;
