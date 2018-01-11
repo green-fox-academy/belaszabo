0df826cbb10185ede016d32a9dbea974da04c7bd
Bela Szabo <belapontszabo@gmail.com>
2018-01-11 12:46:18 +0100

    refact(main) reworked showing first picture and changing pics on thumbnail clicks

--- week-07/day-04/gallery.js
+++ week-07/day-04/gallery.js
@@ -4,13 +4,28 @@ let leftArrow = document.querySelector('.cls-1');
 let rightArrow = document.querySelector('.cls-2');
 let pictures = document.querySelectorAll('img');
 let mainPicture = document.querySelector('.main-image');
-let picPaths = [];
+
+//mukodo verzio, background beallitassal:
+// for (let i = 0; i < pictures.length; i++) {
+//   pictures[i].addEventListener('click', function(event) {
+//     let source = pictures[i].getAttribute('src');
+//     mainPicture.setAttribute('style', 'background-image: url(' + source +')');
+//   });
+// }
+
+let currentPicture = mainPicture.getElementsByTagName('img')[0];
 
 for (let i = 0; i < pictures.length; i++) {
   pictures[i].addEventListener('click', function(event) {
     let source = pictures[i].getAttribute('src');
-    mainPicture.setAttribute('style', 'background-image: url(' + source +')');
+    currentPicture.setAttribute('src', source);
   });
 }
 
+// function nextPic(index) {
+//   mainPicture.removeChild();
+//   mainPicture.appendChild(pictures[index + 1 ]);
+//   currentPicture = pictures[index + 1];
+// }
 
+// showFirstPic();
