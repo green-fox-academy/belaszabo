79c51b615379ece3555b29190b590860c49156b9
Bela Szabo <belapontszabo@gmail.com>
2018-01-16 11:40:38 +0100

    add elevator

--- week-08/day-01/elevator/main.js
+++ week-08/day-01/elevator/main.js
@@ -1,5 +1,30 @@
 class ElevatorController {
 
+  constructor() {
+    this.up = document.querySelector('#up');
+    this.down = document.querySelector('#down');
+    this.add = document.querySelector('#add');
+    this.remove = document.querySelector('#remove');
+    this.elevator = new ElevatorModel(10, 10);
+    this.view = new ElevatorView(this.elevator.people, this.elevator.maxFloor - 1);
+    
+    this.up.addEventListener('click', function() {
+      controller.elevator.moveUp();
+      controller.view.updatePeople();
+    });
+    this.down.addEventListener('click', function() {
+      controller.elevator.moveDown();
+      controller.view.updatePeopleDown();
+    });
+    this.add.addEventListener('click', function() {
+      controller.elevator.addPeople();
+      controller.view.updatePeople();
+    });
+    this.remove.addEventListener('click', function() {
+      controller.elevator.removePeople();
+      controller.view.updatePeople();
+    });
+  }
 }
 
 class ElevatorModel {
@@ -7,7 +32,7 @@ class ElevatorModel {
   constructor(maxFloor, maxPeople) {
     this.maxFloor = maxFloor;
     this.maxPeople = maxPeople;
-    this.elevatorPosition = 0;
+    this.elevatorPosition = this.maxFloor - 1;
     this.elevatorDirection = 'up';
     this.people = 0;
   }
@@ -25,16 +50,16 @@ class ElevatorModel {
   }
 
   moveUp() {
-    if (this.elevatorPosition < this.maxFloor) {
+    if (this.elevatorPosition > 0) {
       this.elevatorDirection = 'up';
-      this.elevatorPosition++;
+      this.elevatorPosition--;
     }
   }
 
   moveDown() {
-    if (this.elevatorPosition > 0) {
+    if (this.elevatorPosition < this.maxFloor - 1) {
       this.elevatorDirection = 'down';
-      this.elevatorPosition--;
+      this.elevatorPosition++;
     }
   }
 
@@ -42,4 +67,37 @@ class ElevatorModel {
 
 class ElevatorView {
 
+  constructor(people, currentFloor) {
+    this.lift = document.querySelector('ul');
+    for (let i = 0; i < 10; i++) {
+      this.lift.innerHTML += '<li></li>'
+    }
+    this.floor = document.getElementsByTagName('li');
+    this.floor[currentFloor].innerText = 0;
+    this.floor[currentFloor].style.backgroundColor = 'green';
+  }
+
+  updatePeople() {
+    if (controller.elevator.elevatorPosition >= 0 && controller.elevator.elevatorPosition <= 9) {
+      if (controller.elevator.elevatorPosition !== 9) {
+        this.floor[controller.elevator.elevatorPosition + 1].innerText = '';
+        this.floor[controller.elevator.elevatorPosition + 1].style.backgroundColor = 'white';
+      }
+      this.floor[controller.elevator.elevatorPosition].innerText = controller.elevator.people;
+      this.floor[controller.elevator.elevatorPosition].style.backgroundColor = 'green';
+    }
+  }
+
+  updatePeopleDown() {
+    if (controller.elevator.elevatorPosition >= 0 && controller.elevator.elevatorPosition <= 9) {
+      if (controller.elevator.elevatorPosition !== 0) {
+        this.floor[controller.elevator.elevatorPosition - 1].innerText = '';
+        this.floor[controller.elevator.elevatorPosition - 1].style.backgroundColor = 'white';
+      }
+      this.floor[controller.elevator.elevatorPosition].innerText = controller.elevator.people;
+      this.floor[controller.elevator.elevatorPosition].style.backgroundColor = 'green';
+    }
+  }
 }
+
+let controller = new ElevatorController();
